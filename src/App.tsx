2fd289c0f605ca1726c3bf378fb67cf0d9353abc
diegosantos <diegosantos3208@gmail.com>
2025-01-19 01:28:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,244 @@
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import * as XLSX from "xlsx";
+
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { useState } from "react";
+import { DataTablePagination } from "@/components/ui/pagination.tsx";
+import { Button } from "@/components/ui/button.tsx";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("email")}</div>
+    ),
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("phone")}</div>
+    ),
+  },
+  {
+    accessorKey: "address",
+    header: "Address",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("address")}</div>
+    ),
+  },
+  {
+    accessorKey: "company",
+    header: "Company",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("company")}</div>
+    ),
+  },
+  {
+    accessorKey: "text",
+    header: "Text",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("text")}</div>,
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("description")}</div>
+    ),
+  },
+  {
+    accessorKey: "job_title",
+    header: "Job Title",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("job_title")}</div>
+    ),
+  },
+];
+
+export default function DataTableDemo() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = React.useState([]);
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      setLoading(true);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const binaryStr = e.target?.result as string;
+        const workbook = XLSX.read(binaryStr, { type: "binary" });
+
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+        const json: unknown = XLSX.utils.sheet_to_json(worksheet);
+
+        setData(json as never);
+        setLoading(false);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }
+
+  function downloadExcel(jsonData: never) {
+    const worksheet = XLSX.utils.json_to_sheet(jsonData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    XLSX.writeFile(workbook, "DataSheet.xlsx");
+  }
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className={"p-12"}>
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        XLSX &gt; JSON
+      </h3>
+
+      <div className={"flex gap-8"}>
+        <Input
+          disabled={loading}
+          type="file"
+          accept=".xlsx"
+          onChange={handleFileUpload}
+          style={{ marginBottom: "20px" }}
+        />
+
+        {!!data?.length && (
+          <Button type={"button"} onClick={() => downloadExcel(data as never)}>
+            Download XLSX
+          </Button>
+        )}
+      </div>
+
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <div className="w-full">
+          <div className="flex items-center py-4">
+            <Input
+              placeholder="Filter Address..."
+              value={
+                (table.getColumn("address")?.getFilterValue() as string) ?? ""
+              }
+              onChange={(event) =>
+                table.getColumn("address")?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
+          </div>
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext(),
+                              )}
+                        </TableHead>
+                      );
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                    >
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+
+          <DataTablePagination table={table} />
+        </div>
+      )}
+    </div>
+  );
+}
